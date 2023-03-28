26b790b32243135f985eb43d1d684cb45501f6cd
Ken Coles <kenscoles@gmail.com>
2023-03-28 11:51:13 +0100

    capitals 1st stab

--- src/app/capitals/capitals.component.ts
+++ src/app/capitals/capitals.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UtilityService } from '../utility.service';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-capitals',
   templateUrl: './capitals.component.html',
   styleUrls: ['./capitals.component.css']
 })
-export class CapitalsComponent {
+export class CapitalsComponent implements OnInit {
 
+  constructor(private http: HttpClient, private util : UtilityService) { }
+  countries: any = [];
+  names: any = [];
+  capitals: any = [];
+  correctOrder=false;
+
+  ngOnInit(): void {
+    this.http.get('/assets/sample.json').subscribe(data => {
+      this.countries = data;
+      this.prepareData();
+    });
+  }
+
+  prepareData(){
+    this.correctOrder=false;
+    this.util.shuffleArray(this.countries);
+    this.names = this.countries.slice(0, 5);// take first 5
+    this.capitals = Array.from(this.names); // clone names array
+    this.util.sattoloCycle(this.capitals); // shuffle it
+    console.log(this.countries);
+    console.log(this.names);
+    console.log(this.capitals);
+
+  }
+  //
+  drop(event: CdkDragDrop<string[]>) {
+    //this.attempts +=1; // increment
+    this.correctOrder = true;
+    moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+
+    /* this.todo.forEach((element: any, index: number) => {
+      if (element.name !== this.done[index].name) {
+        this.correctOrder = false;
+      }
+    });  cannot use break with forEach */
+
+    for (let index = 0; index < this.names.length; index++) {
+      if (this.names[index].name !== this.capitals[index].name) {
+        this.correctOrder = false;
+        break;
+      }
+    }
+    if (this.correctOrder) {
+    console.log("correct!");
+    }
+  }
+  //
 }
---
+++ src/app/utility.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilityService {
+
+  constructor() { }
+  t(min: number, max: number) {
+    return Math.round((min - 0.5) + Math.random() * (max - min + 1));
+  }
+  //
+  getRand(num: number, zeroBased: boolean = true): number {
+    if (zeroBased) {
+      return Math.floor(num * Math.random());
+    }
+    else return 1 + Math.floor(num * Math.random());
+  }
+  // shuffles to any position in new array
+  shuffleArray(array: string[]): string[] {
+    var m = array.length, t, i;
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+
+    return array;
+  }
+  // shuffles but position in new array cannot be same as first
+  sattoloCycle(items: any): void { // avoids duplication of position in array
+    for (var i = items.length; i-- > 1;) {
+      var j = Math.floor(Math.random() * i);
+      var tmp = items[i];
+      items[i] = items[j];
+      items[j] = tmp;
+    }
+  }
+  // sorts an array of objects
+  objsort(obj: any[], prop: string | number, isAsc: 'asc' | 'desc') {
+    console.log('ok')
+    return obj.sort((a, b) => a[prop].toString().localeCompare(b[prop])
+     * (isAsc === "asc" ? 1 : -1));
+  }
+  /*
+  Remove elements from array at given indices - see below for example
+var myArray = ['a','b','c','d','e'];
+    var myDeletions = [0,1];
+    var newArray = this.removeArrayValues(myArray,myDeletions);
+    console.log(newArray)
+  */
+  removeArrayValues<T>(arr:Array<T>, valArray:Array<number>): T[] {
+    valArray = valArray.sort();
+     for (var i = valArray.length - 1; i >= 0; i--)
+       arr.splice(valArray[i], 1);
+ 
+     return arr;
+   }
+
+}
