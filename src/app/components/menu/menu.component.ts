8a017ed94eeae57131b0ee67503e6a388a919d91
Ken Coles <kenscoles@gmail.com>
2024-10-31 10:33:05 +0000

    scss global styling

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, OnInit, inject } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
@@ -12,25 +12,25 @@ import { first } from 'rxjs';
   standalone: true,
   imports: [CommonModule, RouterLink, RouterLink],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css',
+  styleUrl: './menu.component.scss',
 })
 export class MenuComponent implements OnInit {
   state = inject(StateService)
+  private elementRef = inject(ElementRef)
   destroyRef = inject(DestroyRef); // this needs to be declared
   isPhonePortrait = false;
   msg = '';
-  #destroyRef = inject(DestroyRef); // this needs to be declared
 
   constructor(private responsive: BreakpointObserver,
   ) {
     this.state.showMenuButton.set(false) // hides the app comp toolbar
-    const destroyRef = inject(DestroyRef);
-    destroyRef.onDestroy(() => // register a destroy callback
+    //const destroyRef = inject(DestroyRef);
+    this.destroyRef.onDestroy(() => // register a destroy callback
     {
       this.state.showMenuButton.set(true) // shows app comp toolbar when menu closes
       console.log("MENU destroyed")
     }
-    
+
     );
 
   }
@@ -38,8 +38,9 @@ export class MenuComponent implements OnInit {
   ngOnInit(): void {
     //this.screenWidth = window.innerWidth;
     //this.screenHeight = window.innerHeight;
-    this.responsive.observe(['(max-width: 400px)'])//.pipe(first())
-   .pipe(takeUntilDestroyed(this.#destroyRef)) //this.#destroyRef has to be inserted here
+    const breakpoint = getComputedStyle(this.elementRef.nativeElement).getPropertyValue('--breakpointForMobile');
+    this.responsive.observe(`(max-width: ${breakpoint})`)
+      .pipe(takeUntilDestroyed(this.destroyRef)) //this.#destroyRef has to be inserted here
       .subscribe(result => {
         console.log(result)
         if (result.matches) {
@@ -53,11 +54,8 @@ export class MenuComponent implements OnInit {
           this.isPhonePortrait = false;
           this.state.mobile.set(false)
         }
-
       });
   }
- 
-
 }
 
 
