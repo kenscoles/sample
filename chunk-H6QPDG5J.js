80090957fb5b9a78a153ca95a8f03e09997f512c
Ken Coles <kenscoles@gmail.com>
2024-11-19 16:24:46 +0000

    Auto-generated commit [ci skip]

--- chunk-H6QPDG5J.js
+++
@@ -1,1 +0,0 @@
-import{Qa as e,Z as r}from"./chunk-Y4RFIXYV.js";var l=(()=>{let t=class t{constructor(){this.showMenuButton=e(!1),this.mobile=e(!1),console.log("State Service started")}};t.\u0275fac=function(s){return new(s||t)},t.\u0275prov=r({token:t,factory:t.\u0275fac,providedIn:"root"});let o=t;return o})();export{l as a};
