790b0ec200cb1341ca5015969b058f2f7ddecf6d
Ken Coles <kenscoles@gmail.com>
2025-01-08 12:30:31 +0000

    Auto-generated commit [ci skip]

---
+++ chunk-G576EQLH.js
@@ -0,0 +1,1 @@
+import{_ as r,gb as e}from"./chunk-BXJ2JPNR.js";var l=(()=>{let t=class t{constructor(){this.showMenuButton=e(!1),this.mobile=e(!1),console.log("State Service started")}};t.\u0275fac=function(s){return new(s||t)},t.\u0275prov=r({token:t,factory:t.\u0275fac,providedIn:"root"});let o=t;return o})();export{l as a};
